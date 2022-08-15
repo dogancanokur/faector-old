b18a1746d2d72911b35f11a624481e7660c7ea6b
dogancan.okur <dogancan.okur@gmail.com>
2022-08-16 01:24:19 +0300

    init

---
+++ be/src/main/java/net/okur/faector/FaectorApplication.java
@@ -0,0 +1,13 @@
+package net.okur.faector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FaectorApplication.class, args);
+    }
+
+}
---
+++ be/src/test/java/net/okur/faector/FaectorApplicationTests.java
@@ -0,0 +1,13 @@
+package net.okur.faector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
