08407ad6ef7ef46afa7bb3517eecb856e8a55bc0
dogancan.okur <dogancan.okur@gmail.com>
2022-08-21 00:27:25 +0300

    fe - Input component

---
+++ fe/src/components/Input.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Input = (props) => {
+    const {id, label, error, type, name, placeholder, onchange} = props;
+    const className = error ? "form-control is-invalid" : "form-control";
+    return (
+        <div className={"form-group"}>
+            <label htmlFor={id}>{label}</label>
+            <input className={className}
+                   type={type}
+                   id={id}
+                   name={name}
+                   placeholder={placeholder}
+                   onChange={onchange}/>
+            <div className="invalid-feedback">{error}</div>
+        </div>
+    )
+}
+
+export default Input;
--- fe/src/pages/UserSignUpPage.js
+++ fe/src/pages/UserSignUpPage.js
@@ -1,22 +1,17 @@
 import React from "react";
 import {signUp} from "../api/apiCalls";
+import Input from "../components/Input";
 
 class UserSignUpPage extends React.Component {
     state = {
-        username: null,
-        displayName: null,
-        password: null,
-        passwordRepeat: null,
-        pendingApiCall: false,
-        errors: {}
+        username: null, displayName: null, password: null, passwordRepeat: null, pendingApiCall: false, errors: {}
     }
     onChangeInput = (event) => {
         const {name, value} = event.target;
         const errors = {...this.state.errors};
         errors[name] = undefined;
         this.setState({
-            [name]: value,
-            errors
+            [name]: value, errors
         });
     }
 
@@ -38,51 +33,53 @@ class UserSignUpPage extends React.Component {
 
     render() {
         const {pendingApiCall, errors} = this.state;
-        return (
-            <div className={"container"}>
-                <form>
-                    <h1>Sign Up</h1>
-                    <div className={"form-group"}>
-                        <label htmlFor="username">Username</label>
-                        <div className="input-group mb-3">
-                            <div className="input-group-prepend">
-                                <span className="input-group-text" id="basic-addon1">@</span>
-                            </div>
-                            <input className={errors.username ? "form-control is-invalid" : "form-control"} type="text"
-                                   id={"username"} name={"username"}
-                                   placeholder={"Username"} onChange={this.onChangeInput}/>
-                            <div className="invalid-feedback">{errors.username}</div>
+        return (<div className={"container"}>
+            <form>
+                <h1>Sign Up</h1>
+                <div className={"form-group"}>
+                    <label htmlFor="username">Username</label>
+                    <div className="input-group mb-3">
+                        <div className="input-group-prepend">
+                            <span className="input-group-text" id="basic-addon1">@</span>
                         </div>
+                        <input className={errors.username ? "form-control is-invalid" : "form-control"} type="text"
+                               id={"username"} name={"username"}
+                               placeholder={"Username"} onChange={this.onChangeInput}/>
+                        <div className="invalid-feedback">{errors.username}</div>
                     </div>
-                    <div className={"form-group"}>
-                        <label htmlFor="displayName">Display Name</label>
-                        <input className={errors.displayName ? "form-control is-invalid" : "form-control"} type="text"
-                               id={"displayName"} name={"displayName"}
-                               placeholder={"Display Name"} onChange={this.onChangeInput}/>
-                        <div className="invalid-feedback">{errors.displayName}</div>
-                    </div>
-                    <div className={"form-group"}>
-                        <label htmlFor="password">Password</label>
-                        <input className={errors.password ? "form-control is-invalid" : "form-control"} type="password"
-                               id={"password"} name={"password"}
-                               placeholder={"Password"} onChange={this.onChangeInput}/>
-                        <div className="invalid-feedback">{errors.password}</div>
-                    </div>
-                    <div className={"form-group"}>
-                        <label htmlFor="{passwordRepeat}">Password Repeat</label>
-                        <input className="form-control" type="passwordRepeat" id={"passwordRepeat"}
-                               placeholder={"Password Repeat"} name={"passwordRepeat"} onChange={this.onChangeInput}/>
-                    </div>
+                </div>
 
-                    <div className={"text-center"}>
-                        <button className="btn btn-primary" disabled={pendingApiCall} onClick={this.onClickSignUp}>
-                            {pendingApiCall &&
-                                <span className="spinner-border-sm spinner-border mr-1"></span>}Sign Up
-                        </button>
-                    </div>
-                </form>
-            </div>
-        )
+                <Input id={"displayName"}
+                       label={"Display Name"}
+                       error={errors.displayName}
+                       type={"text"}
+                       name={"displayName"}
+                       placeholder={"Display Name"}
+                       onchange={this.onChangeInput}></Input>
+
+                <Input id={"password"}
+                       label={"Password"}
+                       error={errors.password}
+                       type={"password"}
+                       name={"password"}
+                       placeholder={"Password"}
+                       onchange={this.onChangeInput}></Input>
+
+                <Input id={"passwordRepeat"}
+                       label={"Password Repeat"}
+                       error={errors.passwordRepeat}
+                       type={"password"}
+                       name={"password"}
+                       placeholder={"Password Repeat"}
+                       onchange={this.onChangeInput}></Input>
+
+                <div className={"text-center"}>
+                    <button className="btn btn-primary" disabled={pendingApiCall} onClick={this.onClickSignUp}>
+                        {pendingApiCall && <span className="spinner-border-sm spinner-border mr-1"></span>}Sign Up
+                    </button>
+                </div>
+            </form>
+        </div>)
     }
 }
 
