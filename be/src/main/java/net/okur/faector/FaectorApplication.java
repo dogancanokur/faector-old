0b15ecb936477032bd75defb3f1da6d859a8f554
dogancan.okur <dogancan.okur@gmail.com>
2022-08-21 23:42:06 +0300

    be - CommandLineRunner for init user

--- be/src/main/java/net/okur/faector/FaectorApplication.java
+++ be/src/main/java/net/okur/faector/FaectorApplication.java
@@ -1,8 +1,12 @@
 package net.okur.faector;
 
+import net.okur.faector.user.UserRequest;
+import net.okur.faector.user.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class FaectorApplication {
@@ -11,4 +15,14 @@ public class FaectorApplication {
         SpringApplication.run(FaectorApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner createInitialUsers(UserService userService) {
+        return args -> {
+            UserRequest newUser = new UserRequest();
+            newUser.setUsername("user1");
+            newUser.setPassword("passW123");
+            newUser.setDisplayName("User");
+            userService.save(newUser);
+        };
+    }
 }
