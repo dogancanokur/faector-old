b6e5ba7fb6ebe27eff9622263fbeced2869f5b53
dogancan.okur <dogancan.okur@gmail.com>
2022-08-23 02:09:29 +0300

    be - Spring Security - auth user

--- be/src/main/java/net/okur/faector/auth/AuthController.java
+++ be/src/main/java/net/okur/faector/auth/AuthController.java
@@ -1,18 +1,17 @@
 package net.okur.faector.auth;
 
 import com.fasterxml.jackson.annotation.JsonView;
+import net.okur.faector.configuration.FaectorUserDetails;
 import net.okur.faector.shared.Views;
 import net.okur.faector.user.User;
 import net.okur.faector.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Base64;
 
 @RestController
 @RequestMapping("/api/1.0")
@@ -28,12 +27,9 @@ public class AuthController {
 
     @PostMapping("/auth")
     @JsonView(Views.Base.class)
-    ResponseEntity<?> handleAuthentication(@RequestHeader(name = "Authorization", required = false) String authorization) {
-
-        String base64Encoded = authorization.split("Basic ")[1];
-        String decoded = new String(Base64.getDecoder().decode(base64Encoded)); // user1:pass1
-        String[] parts = decoded.split(":"); // ["user1", "pass1"]
-        String username = parts[0]; // user1
+    public ResponseEntity<?> handleAuthentication() {
+        FaectorUserDetails userDetails = (FaectorUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String username = userDetails.getUsername();
         User userInDB = userRepository.findByUsername(username);
         return ResponseEntity.status(HttpStatus.OK).body(userInDB);
     }
