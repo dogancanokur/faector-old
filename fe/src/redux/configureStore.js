6496442c97c04f88db7114ae28b1cb7a1e409d6d
dogancan.okur <dogancan.okur@gmail.com>
2022-09-18 18:20:09 +0300

    fe - localStorage

--- fe/src/redux/configureStore.js
+++ fe/src/redux/configureStore.js
@@ -1,23 +1,28 @@
 import {createStore} from "redux";
 import authReducer from "./authReducer";
 
-const loggedInState = {
-    // isLoggedIn: false,
-    // loggedUsername: undefined,
-    // displayName: undefined,
-    // image: undefined,
-    // password: undefined
-    isLoggedIn: true,
-    loggedUsername: "user1",
-    displayName: "Display 1",
-    image: null,
-    password: "psaf"
-}
-
 const configureStore = () => {
-    return createStore(authReducer, loggedInState,
-        (window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()) // for debugger
-    );
+    const authData = localStorage.getItem('faector-auth');
+    let stateInLocalStorage = {
+        isLoggedIn: false,
+        loggedUsername: undefined,
+        displayName: undefined,
+        image: undefined,
+        password: undefined
+    }
+    if (authData) {
+        try {
+            stateInLocalStorage = JSON.parse(authData);
+        } catch (e) {
+            console.warn('LocalStorage value error\n', e);
+        }
+    }
+    const store = createStore(authReducer, stateInLocalStorage,
+        (window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()) /*for debugger*/)
+    store.subscribe(() => {
+        localStorage.setItem('faector-auth', JSON.stringify(store.getState()))
+    })
+    return store;
 };
 
 export default configureStore;
